a78f2e99c48256c9344c59db6160dffaf0b2bcbb
tarathep <tarathep.choengchaiyaphum@global.ntt>
2025-06-09 11:58:32 +0000

    3.5 Creating Our First DTO

---
+++ src/users/dtos/create-user.dto.ts
@@ -0,0 +1,27 @@
+import { IsEmail, IsNotEmpty, IsOptional, IsString, Matches, MaxLength, MinLength } from "class-validator";
+
+export class CreateUserDto {
+    @IsString()
+    @IsNotEmpty()
+    @MinLength(3)
+    @MaxLength(96)
+    firstName: string;
+
+    @IsString()
+    @IsOptional()
+    @MinLength(3)
+    @MaxLength(96)
+    lastName?: string;
+
+    @IsEmail()
+    @IsNotEmpty()
+    email: string;
+
+    @IsString()
+    @IsNotEmpty()
+    @MinLength(8)
+    @Matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/,{
+        message: 'Password must contain at least one uppercase letter, one lowercase letter, and one number.',
+    })
+    password: string;
+}
