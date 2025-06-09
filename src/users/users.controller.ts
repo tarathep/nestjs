0cc69ee9ee52188e7cff8689e951374a28d0db46
tarathep <tarathep.choengchaiyaphum@global.ntt>
2025-06-09 09:03:40 +0000

    2.9 Params, Query and Body and Additional Request Components

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,17 +1,36 @@
-import { Controller, Get, Post, Patch, Put, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Patch, Put, Delete,Param, Query, ParseIntPipe, Body,Headers, Ip } from '@nestjs/common';
 
 @Controller('users')
 export class UsersController {
 
-    @Get()
-    public getUsers() {
-        return "You sent a get request to users endpoint";
+    @Get('/:id/{:optional}')
+    public getUsers(
+        @Param('id', ParseIntPipe) id: number | undefined,
+        @Param('optional') optional?: number | undefined,
+        @Query('limit', ParseIntPipe) limit?: number
+    ) {
+        console.log(typeof id);
+        console.log(typeof limit);
+        console.log(optional);
+
+        if (optional) {
+        return `ID is ${id} and optional parameter is ${optional}`;
+        } else {
+        return `ID is ${id} and no optional parameter`;
+        }
     }
 
     @Post()
-    public createUser() {
+    public createUser(
+        @Body() request: any,
+        @Headers() headers: any,
+        @Ip() ip: any,
+    ) {
+        console.log(request);
+        console.log(headers);
+        console.log(ip);
         return 'You sent a post request to users endpoint';
     }
 
-    
+
 }
