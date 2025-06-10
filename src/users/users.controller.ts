7e7f99671146223e6a624b316f13c7406d0f6f59
tarathep <tarathep.choengchaiyaphum@global.ntt>
2025-06-10 08:27:31 +0000

    3.9 Using DTOs with Params

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,17 +1,17 @@
 import { Controller, Get, Post, Patch, Put, Delete,Param, Query, ParseIntPipe, Body,Headers, Ip, DefaultValuePipe, ValidationPipe } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
+import { GetUsersParamDto } from './dtos/get-users-param.dto';
 
 @Controller('users')
 export class UsersController {
 
     @Get('/:id/')
     public getUsers(
-        @Param('id', ParseIntPipe) id: number | undefined,
+        @Param() getUsersParamDto: GetUsersParamDto,
         @Query('limit',new DefaultValuePipe(10), ParseIntPipe) limit?: number,
         @Query('page',new DefaultValuePipe(1) ,ParseIntPipe) page?: number,
     ) {
-        console.log( limit);
-        console.log( page);
+       console.log(getUsersParamDto);
 
         return `You sent a get request to users endpoint`;
         
