627dbef78ee5367e0a15c2fe26322a89f4d82e78
tarathep <tarathep.choengchaiyaphum@global.ntt>
2025-06-11 07:44:37 +0000

    4.4 findall Users Method

--- src/users/providers/users.service.ts
+++ src/users/providers/users.service.ts
@@ -1,6 +1,23 @@
 import { Injectable } from '@nestjs/common';
+import { GetUsersParamDto } from '../dtos/get-users-param.dto';
 
 @Injectable()
 export class UsersService {
 
+    public findAll(
+        getUsersParamDto: GetUsersParamDto,
+        limit: number,
+        page: number,
+    ) {
+        return [
+            {
+                firstName: 'John',
+                email: 'john@doe.com'
+            },
+            {
+                firstName: 'Alice',
+                email: 'alice@doe.com'
+            }
+        ]
+    }
 }
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -18,9 +18,13 @@ export class UsersController {
         @Query('limit',new DefaultValuePipe(10), ParseIntPipe) limit?: number,
         @Query('page',new DefaultValuePipe(1) ,ParseIntPipe) page?: number,
     ) {
-       console.log(getUsersParamDto);
+       
 
-        return `You sent a get request to users endpoint`;
+        return this.usersService.findAll(
+            getUsersParamDto,
+            limit ?? 10,
+            page ?? 1
+        );
         
     }
 
