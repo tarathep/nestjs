d814b584215766474ac2b4443481af91495f88a9
tarathep <tarathep.choengchaiyaphum@global.ntt>
2025-06-22 15:59:50 +0000

    5.7 Adding Validations To CreatePostDto

--- src/posts/dto/create-post.dto.ts
+++ src/posts/dto/create-post.dto.ts
@@ -1,15 +1,50 @@
+import { IsArray, IsEnum, IsISO8601, IsJSON, IsNotEmpty, IsOptional, IsString, IsUrl, isURL, Matches, Min, MinLength } from "class-validator";
 import { PostStatus } from "../enums/postStatus.enum";
 import { PostType } from "../enums/postType.enum";
 
 export class CreatePostDto {
+    @IsString()
+    @MinLength(4)
+    @IsNotEmpty()
     title: string;
+
+    
+    @IsEnum(PostType)
+    @IsNotEmpty()
     postType: PostType;
+
+    @IsString()
+    @IsNotEmpty()
+    @Matches(/^[a-z0-9]+(?:-[a-z0-9]+)*$/, {
+        message: 'A slug should be all small letters and use only "-" and without spaces. For example: "my-url"',
+    })
     slug: string;
+
+    @IsEnum(PostStatus)
+    @IsNotEmpty()
     status: PostStatus;
+
+    @IsString()
+    @IsOptional()
     content?: string;
+
+    @IsOptional()
+    @IsJSON()
     schema?: string;
+
+    @IsOptional()
+    @IsUrl()
     featuredImageUrl?: string;
+
+    @IsOptional()
+    @IsISO8601()
     publishOn?: Date;
+
+    @IsOptional()
+    @IsArray()
+    @IsString({ each: true }) // Ensure each tag is a string
+    @MinLength(3, { each: true }) // Minimum length for each tag
     tags?: string[];
+
     metaOption:[{key: 'sidebarEnabled'; value: true}];
 }
