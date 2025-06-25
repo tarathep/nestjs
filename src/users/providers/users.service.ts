1c77fb112dd4eb4dbde5a5c07660723e8f93a7f2
tarathep <tarathep.choengchaiyaphum@nttdata.com>
2025-06-25 11:09:33 +0000

    5.13 Compodoc Coverage and JSDocs

--- src/users/providers/users.service.ts
+++ src/users/providers/users.service.ts
@@ -2,14 +2,30 @@ import { forwardRef, Inject, Injectable } from '@nestjs/common';
 import { GetUsersParamDto } from '../dtos/get-users-param.dto';
 import { AuthService } from 'src/auth/providers/auth.service';
 
+/**
+ * Class to connect to Users table and perform business operations
+ */
 @Injectable()
 export class UsersService {
 
+    /**
+     * Constructor to inject the AuthService.
+     * Using forwardRef to avoid circular dependency issues.
+     * 
+     * @param authService 
+     */
     constructor(
         //Inejcting Auth service
         @Inject(forwardRef(() => AuthService))
         private readonly authService: AuthService) {}
 
+    /**
+     * The method to get all users from the database.
+     * @param getUsersParamDto 
+     * @param limit 
+     * @param page 
+     * @returns 
+     */
     public findAll(
         getUsersParamDto: GetUsersParamDto,
         limit: number,
@@ -31,6 +47,11 @@ export class UsersService {
         ]
     }
 
+    /**
+     * Find a single user using the ID of the user.
+     * @param id 
+     * @returns 
+     */
     public findOneById(id: string) {
         return {
             id: 1234,
