3b64eeaeff097776024446024babab5b4f121207
tarathep <tarathep.choengchaiyaphum@global.ntt>
2025-07-03 08:33:07 +0000

    6.7 Creating Our First Entity - user.entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { UsersModule } from './users/users.module';
 import { PostsModule } from './posts/posts.module';
 import { AuthModule } from './auth/auth.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './users/user.entity';
 
 @Module({
   imports: [UsersModule, PostsModule, AuthModule, TypeOrmModule.forRootAsync({
@@ -12,7 +13,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
     inject: [],
     useFactory: () => ({
       type: 'postgres',
-      entities: [],
+      entities: [User],
       synchronize: true,
       port: 5432,
       username: 'postgres',
